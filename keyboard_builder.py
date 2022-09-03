6a25351658a55e364c704dbd4754027393b692d0
avdyck <alexander.vandyck@conundra.eu>
2022-09-03 23:41:00 +0200

    xmgbz

--- keyboard_builder.py
+++ keyboard_builder.py
@@ -1,76 +1,73 @@
-import math
 import random
 
 import numpy as np
 
 qwerty = [
     'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ' ',
-    'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ':',
+    'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',
+    '?', ' ',
 ]
 
-optimized = [
-    'q', 'p', 'u', 'o', 'x', 'f', 'l', 'r', 'd', 'b',
-    'c', 'i', 'e', 'a', 'g', 'v', 'n', 't', 's', 'w',
-    'z', ';', '<', '>', '?', 'j', 'h', 'm', 'k', 'y',
+ytrewq = [
+    'p', 'o', 'i', 'u', 'y', 't', 'r', 'e', 'w', 'q',
+    ':', 'l', 'k', 'j', 'h', 'g', 'f', 'd', 's', 'a',
+    '/', '.', ',', 'm', 'n', 'b', 'v', 'c', 'x', 'z',
+    '?', ' ',
 ]
 
 custom1 = [
-    'v', 'p', 'u', 'o', 'q', 'f', 'l', 'r', 'd', 'y',
-    'k', 'i', 'e', 'a', 'j', 'm', 'n', 't', 's', 'c',
-    'z', '.', '.', '.', '.', 'x', 'h', 'w', 'g', 'b',
+    'x', 'p', 'u', 'o', 'q', 'f', 'l', 'r', 'd', 'y',
+    'k', 'i', 'e', 'a', 'b', 'm', 'n', 't', 's', 'c',
+    'z', '.', '.', '.', '.', 'v', 'h', 'w', 'g', 'j',
+    '?', ' '
 ]
 
 custom2 = [
-    'y', 'o', 'u', 'g', 'q', 'w', 'c', 'l', 'h', 'z',
-    'i', 'a', 'e', 't', 'k', 'f', 'd', 'r', 'n', 's',
-    '.', '.', '.', 'x', '.', 'b', 'p', 'j', 'm', 'v',
-]
-
-bruh_keyboard = [
-    'b', 'r', 'u', 'h', 'x', 'f', 'd', 'o', 'p', 'y',
-    'c', 's', 'e', 'n', 'l', 'g', 't', 'a', 'i', 'w',
-    'q', 'j', ';', 'm', 'z', 'v', 'k', '<', '>', '?',
+    'x', 'm', 'g', 'b', 'z', 'q', 'f', 'u', 'o', 'y',
+    'r', 'l', 's', 't', 'p', 'k', 'n', 'e', 'a', 'i',
+    'c', 'j', 'w', 'd', 'v', ':', 'h', ',', '.', '/',
+    '?', ' '
 ]
 
 hand = [
     0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
     0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
     0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
+    2, 2,
 ]
 
 fingers = [
-    0, 1, 2, 3, 3, 4, 4, 5, 6, 7,
-    0, 1, 2, 3, 3, 4, 4, 5, 6, 7,
-    0, 1, 3, 3, 3, 4, 4, 5, 6, 7,
+    0, 1, 2, 3, 3, 3, 3, 2, 1, 0,
+    0, 1, 2, 3, 3, 3, 3, 2, 1, 0,
+    0, 1, 2, 3, 3, 3, 3, 2, 1, 0,
+    8, 8,
 ]
 
 rows = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
     2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+    3, 3,
 ]
 
-keys = qwerty
-keymap = {k: i for i, k in enumerate(keys)}
-qwertymap = {k: i for i, k in enumerate(qwerty)}
+keymap = {k: i for i, k in enumerate(qwerty)}
 
-fixed_keys = set(keymap[k] for k in './,qzxcv')
-vim_keys = set(qwertymap[k] for k in '')
-vimmable_keys = set(keymap[k] for k in 'hjkl')
-space = keymap[' ']
-
-# effort = np.array([
-#     2.0, 1.1, 1.05, 1.0, 1.9, 1.9, 1.0, 1.05, 1.1, 2.0,
-#     1.0, 0.7, 0.65, 0.6, 1.5, 1.5, 0.6, 0.65, 0.7, 1.0,
-#     1.8, 1.4, 1.35, 1.3, 1.7, 1.7, 1.3, 1.35, 1.4, 1.8,
-# ], dtype=float)
+fixed_keys = set(keymap[k] for k in ' ?')
 
 effort = np.array([
-    9.9, 1.0, 1.0, 1.0, 5.0, 5.0, 1.0, 1.0, 1.0, 9.9,
-    5.0, 0.5, 0.5, 0.5, 1.5, 1.5, 0.5, 0.5, 0.5, 5.0,
-    7.0, 2.0, 5.0, 1.5, 7.0, 7.0, 1.5, 5.0, 2.0, 7.0,
+    5.0, 1.1, 1.0, 2.0, 3.0, 0, 0, 0, 0, 0,
+    0.8, 0.7, 0.6, 0.5, 1.8, 0, 0, 0, 0, 0,
+    1.6, 2.2, 2.0, 1.0, 3.0, 0, 0, 0, 0, 0,
+    0.0, 0.0,
 ], dtype=float)
+
+# effort = np.array([
+#     9.9, 1.0, 1.0, 1.0, 5.0, 0, 0, 0, 0, 0,
+#     1.0, 0.6, 0.5, 0.4, 1.5, 0, 0, 0, 0, 0,
+#     2.5, 2.0, 5.0, 1.5, 7.0, 0, 0, 0, 0, 0,
+#     0, 0
+# ], dtype=float)
 
 # effort = np.array([
 #     3.0, 2.4, 2.0, 2.2, 3.2, 3.2, 2.0, 2.0, 2.4, 3.0,
@@ -79,148 +76,189 @@ effort = np.array([
 # ], dtype=float)
 
 
-digram_effort = np.zeros((len(keys), len(keys)), dtype=float)
-trigram_effort = np.zeros((len(keys), len(keys), len(keys)), dtype=float)
+digram_effort = np.zeros((len(qwerty), len(qwerty)), dtype=float)
+trigram_effort = np.zeros((len(qwerty), len(qwerty), len(qwerty)), dtype=float)
 
 a = 0.3
 b = 0.4
-c = 0.5
-d = 0.7
+c = 0.6
+d = 1.0
+e = 1.5
+f = 2.0
 
-e = 2.0
-f = 2.2
-g = 2.4
-h = 2.6
+m = 2.0
+n = 3.0
+o = 4.0
+p = o
 
 z = 0.5
 roll_map = np.array([
     [  # q
-        e, a, b, b, c, z, z, z, z, z,
-        f, d, d, c, c, z, z, z, z, z,
-        g, g, g, d, d, z, z, z, z, z,
+        m, a, a, a, a, z, z, z, z, z,
+        n, d, d, c, b, z, z, z, z, z,
+        o, f, f, d, d, z, z, z, z, z,
+        z, z,
     ], [  # w
-        0, e, a, b, b, z, z, z, z, z,
-        c, f, c, b, c, z, z, z, z, z,
-        g, g, g, c, d, z, z, z, z, z,
+        0, m, a, a, a, z, z, z, z, z,
+        c, n, c, a, b, z, z, z, z, z,
+        f, o, f, c, d, z, z, z, z, z,
+        z, z,
     ], [  # e
-        0, 0, e, a, b, z, z, z, z, z,
-        c, d, f, b, b, z, z, z, z, z,
-        f, g, g, c, c, z, z, z, z, z,
+        0, 0, m, a, b, z, z, z, z, z,
+        b, d, n, a, c, z, z, z, z, z,
+        d, f, o, c, d, z, z, z, z, z,
+        z, z,
     ], [  # r
-        0, 0, 0, e, f, z, z, z, z, z,
-        b, b, c, f, g, z, z, z, z, z,
-        c, f, g, g, h, z, z, z, z, z,
+        0, 0, 0, m, n, z, z, z, z, z,
+        b, c, d, n, o, z, z, z, z, z,
+        c, d, e, o, p, z, z, z, z, z,
+        z, z,
     ], [  # t
-        0, 0, 0, 0, e, z, z, z, z, z,
-        b, c, c, g, f, z, z, z, z, z,
-        c, f, g, h, g, z, z, z, z, z,
+        0, 0, 0, 0, m, z, z, z, z, z,
+        b, c, c, o, n, z, z, z, z, z,
+        c, d, e, p, o, z, z, z, z, z,
+        z, z,
     ], [  # y
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         z, z, z, z, z, 0, 0, 0, 0, 0,
         z, z, z, z, z, 0, 0, 0, 0, 0,
+        z, z,
     ], [  # u
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         z, z, z, z, z, 0, 0, 0, 0, 0,
         z, z, z, z, z, 0, 0, 0, 0, 0,
+        z, z,
     ], [  # i
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         z, z, z, z, z, 0, 0, 0, 0, 0,
         z, z, z, z, z, 0, 0, 0, 0, 0,
+        z, z,
     ], [  # o
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         z, z, z, z, z, 0, 0, 0, 0, 0,
         z, z, z, z, z, 0, 0, 0, 0, 0,
+        z, z,
     ], [  # p
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         z, z, z, z, z, 0, 0, 0, 0, 0,
         z, z, z, z, z, 0, 0, 0, 0, 0,
+        z, z,
     ], [  # a
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        e, a, b, b, b, z, z, z, z, z,
-        f, d, d, c, c, z, z, z, z, z,
+        m, a, a, a, a, z, z, z, z, z,
+        n, d, c, b, c, z, z, z, z, z,
+        z, z,
     ], [  # s
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, e, a, b, c, z, z, z, z, z,
-        1, f, d, c, c, z, z, z, z, z,
+        0, m, a, a, c, z, z, z, z, z,
+        c, n, c, b, c, z, z, z, z, z,
+        z, z,
     ], [  # d
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, e, a, b, z, z, z, z, z,
-        c, d, f, b, c, z, z, z, z, z,
+        0, 0, m, a, c, z, z, z, z, z,
+        c, d, n, b, c, z, z, z, z, z,
+        z, z,
     ], [  # f
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, e, f, z, z, z, z, z,
-        c, c, d, f, g, z, z, z, z, z,
+        0, 0, 0, m, n, z, z, z, z, z,
+        b, c, d, n, o, z, z, z, z, z,
+        z, z,
     ], [  # g
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, e, z, z, z, z, z,
-        c, d, d, g, f, z, z, z, z, z,
+        0, 0, 0, 0, m, z, z, z, z, z,
+        c, d, d, o, n, z, z, z, z, z,
+        z, z,
     ], [  # h
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         z, z, z, z, z, 0, 0, 0, 0, 0,
+        z, z,
     ], [  # j
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         z, z, z, z, z, 0, 0, 0, 0, 0,
+        z, z,
     ], [  # k
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         z, z, z, z, z, 0, 0, 0, 0, 0,
+        z, z,
     ], [  # l
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         z, z, z, z, z, 0, 0, 0, 0, 0,
+        z, z,
     ], [  # ;
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         z, z, z, z, z, 0, 0, 0, 0, 0,
+        z, z,
     ], [  # z
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        e, a, b, b, c, z, z, z, z, z,
+        m, a, a, a, b, z, z, z, z, z,
+        z, z,
     ], [  # x
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, e, a, b, c, z, z, z, z, z,
+        0, m, a, b, d, z, z, z, z, z,
+        z, z,
     ], [  # c
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, e, a, b, z, z, z, z, z,
+        0, 0, m, a, b, z, z, z, z, z,
+        z, z,
     ], [  # v
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, e, f, z, z, z, z, z,
+        0, 0, 0, m, n, z, z, z, z, z,
+        z, z,
     ], [  # b
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, e, z, z, z, z, z,
+        0, 0, 0, 0, m, z, z, z, z, z,
+        z, z,
     ], [  # n
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        z, z,
     ], [  # m
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        z, z,
     ], [  # ,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        z, z,
     ], [  # .
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        z, z,
     ], [  # /
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        z, z,
+    ], [  # spc
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        m, z,
+    ], [  # spc
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, m,
     ]
 ], dtype=float)
 
-letter_frequency: np.ndarray = np.zeros(len(keys), dtype=float)
-digram_frequency: np.ndarray = np.zeros((len(keys), len(keys)), dtype=float)
-trigram_frequency: np.ndarray = np.zeros((len(keys), len(keys), len(keys)), dtype=float)
-
+letter_frequency: np.ndarray = np.zeros(len(qwerty), dtype=float)
+digram_frequency: np.ndarray = np.zeros((len(qwerty), len(qwerty)), dtype=float)
+trigram_frequency: np.ndarray = np.zeros((len(qwerty), len(qwerty), len(qwerty)), dtype=float)
 
 def initialize():
     def initialize_letters():
@@ -242,30 +280,45 @@ def initialize():
                 trigram_frequency[keymap[l0], keymap[l1], keymap[l2]] = float(f)
 
     def initialize_roll_map():
-        i = np.tril_indices_from(roll_map)
-        roll_map[i] = roll_map.T[i]
+        for i0, k0 in enumerate(qwerty):
+            for i1, k1 in enumerate(qwerty):
+                if i1 < i0:
+                    assert roll_map[i0][i1] == 0
+                    roll_map[i0][i1] = roll_map[i1][i0]
 
-        for i in range(3):
-            for l in range(3):
-                for j, k in ((0, 9), (1, 8), (2, 7), (3, 6), (4, 5)):
-                    roll_map[k + 10 * l][5 + 10 * i:10 + 10 * i] = roll_map[j + 10 * l][0 + 10 * i:5 + 10 * i][::-1]
+        for i0, k0 in enumerate(qwerty):
+            mirror_i0 = ytrewq.index(k0)
+            if hand[i0] == 0:
+                effort[mirror_i0] = effort[i0]
 
-        tmp_keymap = {k: i for i, k in enumerate(qwerty)}
-        assert roll_map[tmp_keymap['p'], tmp_keymap['k']] == roll_map[tmp_keymap['q'], tmp_keymap['d']]
-        assert roll_map[tmp_keymap['v'], tmp_keymap['e']] == roll_map[tmp_keymap['m'], tmp_keymap['i']]
-        assert roll_map[tmp_keymap['y'], tmp_keymap['p']] == roll_map[tmp_keymap['t'], tmp_keymap['q']]
+                for i1, k1 in enumerate(qwerty):
+                    mirror_i1 = ytrewq.index(k1)
 
-        for i0 in range(len(keys)):
-            for i1 in range(len(keys)):
+                    if hand[i1] == 0:
+                        roll_map[mirror_i0][mirror_i1] = roll_map[i0][i1]
+
+        assert roll_map[keymap['p'], keymap['k']] == \
+               roll_map[keymap['q'], keymap['d']] == \
+               roll_map[keymap['k'], keymap['p']] == \
+               roll_map[keymap['d'], keymap['q']]
+
+        # # favor inward rolls slightly
+        # for i0 in range(len(qwerty)):
+        #     for i1 in range(len(qwerty)):
+        #         if hand[i0] == hand[i1] and fingers[i0] < fingers[i1]:
+        #             roll_map[i0][i1] *= 0.75
+
+        for i0 in range(len(qwerty)):
+            for i1 in range(len(qwerty)):
                 digram_effort[i0][i1] = roll_map[i0][i1] * (effort[i0] + effort[i1])
 
     def initialize_tri_roll_map():
 
-        for i0 in range(len(keys)):
-            for i1 in range(len(keys)):
-                for i2 in range(len(keys)):
+        for i0 in range(len(qwerty)):
+            for i1 in range(len(qwerty)):
+                for i2 in range(len(qwerty)):
 
-                    tri_effort = (0.4 * digram_effort[i0, i1] + 0.4 * digram_effort[i1, i2] + 0.2 * digram_effort[i0, i2])
+                    tri_effort = digram_effort[i0, i1] + digram_effort[i1, i2] + 0.5 * digram_effort[i0, i2]
 
                     # hand pingpong good case
                     if hand[i0] == hand[i1] == hand[i2]:
@@ -296,7 +349,7 @@ class Keyboard:
             self.keyboard = other.keyboard.copy()
             self.penalty = other.penalty
         else:
-            self.keyboard = np.array(range(len(keys)), dtype=int)
+            self.keyboard = np.array(range(len(qwerty)), dtype=int)
             # np.random.shuffle(self.keyboard)
 
             changed = True
@@ -305,16 +358,6 @@ class Keyboard:
                 for i, k in enumerate(self.keyboard):
                     if i != k and k in fixed_keys:
                         self.apply_neighbor(i, k)
-                        changed = True
-
-                should_be_vim_keys = []
-                for i in vim_keys:
-                    if self.keyboard[i] not in vimmable_keys:
-                        should_be_vim_keys.append(i)
-
-                for i, k in enumerate(self.keyboard):
-                    if should_be_vim_keys and k in vimmable_keys and i not in vim_keys:
-                        self.apply_neighbor(i, should_be_vim_keys.pop())
                         changed = True
 
             self.calculate_penalties()
@@ -332,36 +375,21 @@ class Keyboard:
                       if k not in fixed_keys]
 
         i0 = random.choice(candidates)
-
-        if i0 in vim_keys:
-            candidates = [i1 for i1, k in enumerate(self.keyboard)
-                          if i1 != i0
-                          if k in vimmable_keys]
-
-        else:
-            candidates = [i1 for i1, k in enumerate(self.keyboard)
-                          if k not in fixed_keys
-                          if i1 not in vim_keys
-                          if i1 != i0]
+        candidates = [i1 for i1, k in enumerate(self.keyboard)
+                      if k not in fixed_keys
+                      if i1 != i0]
 
         i1 = random.choice(candidates)
-        if self.keyboard[i0] not in vimmable_keys and i1 in vim_keys:
-            raise Exception
-
         return i0, i1
 
     def calculate_penalties(self):
         # letter
         kb = self.keyboard
-        # letter_delta = np.sum(letter_frequency[kb] * effort)
 
-        # digram
-        # digram_delta = np.sum(digram_effort * digram_frequency[kb][:, kb])
-
-        # trigram
-        trigram_delta = np.sum(trigram_effort * trigram_frequency[kb][:, kb][:, :, kb])
-
-        self.penalty = trigram_delta
+        self.penalty = 0 \
+                       + np.sum(letter_frequency[kb] * effort) \
+                       + np.sum(digram_effort * digram_frequency[kb][:, kb]) \
+                       + np.sum(trigram_effort * trigram_frequency[kb][:, kb][:, :, kb])
 
     def score_and_apply_neighbor(self, i1: int, i2: int):
         self.apply_neighbor(i1, i2)
@@ -415,17 +443,17 @@ class Keyboard:
     def __str__(self):
         result = []
         for k in self.keyboard[:10]:
-            result.append(keys[k])
+            result.append(qwerty[k])
             result.append('   ')
         result.pop()
         result.append('\n')
         for k in self.keyboard[10:20]:
-            result.append(keys[k])
+            result.append(qwerty[k])
             result.append('   ')
         result.pop()
         result.append('\n')
-        for k in self.keyboard[20:]:
-            result.append(keys[k])
+        for k in self.keyboard[20:30]:
+            result.append(qwerty[k])
             result.append('   ')
         result.pop()
         return ''.join(result)
